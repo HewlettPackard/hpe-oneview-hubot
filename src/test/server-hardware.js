d38edecb75fc0ba671cf8ab5e236c5c91a862bed
kelseycribari <kelsey.cribari@gmail.com>
2017-06-21 11:18:03 -0600

    Adding testing to increase coverage (#97)
    * Test functionality for server-hardware.
    
    * Added testing for server profiles
    
    * Final changes to server profiles testing
    
    * Remove commented code

---
+++ src/test/server-hardware.js
@@ -0,0 +1,176 @@
+/*
+(c) Copyright 2016-2017 Hewlett Packard Enterprise Development LP
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+
+import OVClient from '../oneview-sdk/ov-client';
+import ServerHardware from '../oneview-sdk/server-hardware';
+
+let chai = require('chai');
+let sinon = require('sinon');
+let nock = require('nock');
+let assert = require('assert');
+let Bluebird = require('bluebird');
+
+
+describe('ServerHardware', () => {
+  let oVClient;
+  var serverHardware;
+  beforeEach(() => {
+    let oneviewConfig = {
+      applianceIp: 'localhost',
+      apiVersion: 300,
+      readOnly: true,
+      pollingInterval: 60,
+      notificationsRoom: 'room'
+    };
+    oVClient = new OVClient(oneviewConfig, {});
+    serverHardware = new ServerHardware(oVClient);
+  });
+
+  it('get "off" hardware', sinon.test(function() {
+
+    let serverHardwareResponse = {
+      "type": "server-hardware-list",
+      "category": "server-hardware",
+      "count": 1,
+      "members": [
+          {
+              "type": "server-hardware",
+              "name": "0000A6610EE, bay 5",
+              "powerState": "Off",
+              "status": "Critical",
+          }]
+    };
+
+    sinon.stub(oVClient.connection, '__http__').returns(Bluebird.resolve(serverHardwareResponse));
+    return serverHardware.getHardwareByPowerState("Off").then(function(data) {
+      data.members[0].powerState.should.equal("Off");
+    });
+  }));
+
+
+  it('get "on" hardware', sinon.test(function() {
+
+    let serverHardwareResponse = {
+      "type": "server-hardware-list",
+      "category": "server-hardware",
+      "count": 1,
+      "members": [
+          {
+            "type": "server-hardware",
+            "name": "0000A6710EE, bay 3",
+            "powerState": "On",
+            "status": "OK",
+          }]
+    };
+
+    sinon.stub(oVClient.connection, '__http__').returns(Bluebird.resolve(serverHardwareResponse));
+    return serverHardware.getHardwareByPowerState("On").then(function(data) {
+      data.type.should.equal("server-hardware-list");
+      data.members[0].powerState.should.equal("On");
+    });
+  }));
+
+  it('get critical hardware', sinon.test(function() {
+    let serverHardwareResponse = {
+      "type": "server-hardware-list",
+      "category": "server-hardware",
+      "count": 1,
+      "members": [
+          {
+              "type": "server-hardware",
+              "name": "0000A6610EE, bay 5",
+              "powerState": "Off",
+              "status": "Critical",
+          }]
+    };
+    sinon.stub(oVClient.connection, '__http__').returns(Bluebird.resolve(serverHardwareResponse));
+
+    return serverHardware.getHardwareByStatus("Critical").then(function(data) {
+      data.members[0].status.should.equal("Critical");
+    });
+  }));
+
+  it('get warning hardware', sinon.test(function() {
+    let serverHardwareResponse = {
+      "type": "server-hardware-list",
+      "category": "server-hardware",
+      "count": 1,
+      "members": [
+          {
+            "type": "server-hardware",
+            "name": "0000A6710EF, bay 2",
+            "powerState": "On",
+            "status": "Warning",
+          }]
+    };
+    sinon.stub(oVClient.connection, '__http__').returns(Bluebird.resolve(serverHardwareResponse));
+
+    serverHardware.getHardwareByStatus("Warning").then(function(data) {
+      data.members[0].status.should.equal("Warning");
+    });
+  }));
+
+  it('get disabled hardware', sinon.test(function() {
+    let serverHardwareResponse = {
+      "type": "server-hardware-list",
+      "category": "server-hardware",
+      "count": 1,
+      "members": [
+          {
+            "type": "server-hardware",
+            "name": "0000A6710EA, bay 7",
+            "powerState": "On",
+            "status": "Disabled",
+          }]
+    };
+
+    sinon.stub(oVClient.connection, '__http__').returns(Bluebird.resolve(serverHardwareResponse));
+
+    serverHardware.getHardwareByStatus("Disabled").then(function(data) {
+      data.members[0].status.should.equal("Disabled");
+    });
+  }));
+
+  it('get OK hardware', sinon.test(function() {
+    let serverHardwareResponse = {
+      "type": "server-hardware-list",
+      "category": "server-hardware",
+      "count": 1,
+      "members": [
+          {
+            "type": "server-hardware",
+            "name": "0000B6710EA, bay 7",
+            "powerState": "On",
+            "status": "OK",
+          }]
+    };
+
+    sinon.stub(oVClient.connection, '__http__').returns(Bluebird.resolve(serverHardwareResponse));
+
+    serverHardware.getHardwareByStatus("OK").then(function(data) {
+      data.members[0].status.should.equal("OK");
+    });
+  }));
+
+
+});
---
+++ src/test/server-profiles.js
@@ -0,0 +1,126 @@
+/*
+(c) Copyright 2016-2017 Hewlett Packard Enterprise Development LP
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+import OVClient from '../oneview-sdk/ov-client';
+import ServerProfiles from '../oneview-sdk/server-profiles';
+
+let chai = require('chai');
+let sinon = require('sinon');
+let nock = require('nock');
+let assert = require('assert');
+let Bluebird = require('bluebird');
+
+
+describe('ServerProfiles', () => {
+  let oVClient;
+  var serverProfiles;
+  beforeEach(() => {
+    let oneviewConfig = {
+      applianceIp: 'localhost',
+      apiVersion: 300,
+      readOnly: true,
+      pollingInterval: 60,
+      notificationsRoom: 'room'
+    };
+    oVClient = new OVClient(oneviewConfig, {});
+    serverProfiles = new ServerProfiles(oVClient);
+  });
+
+  it('get critical profiles', sinon.test(function() {
+    let serverProfileResponse = {
+      "type": "server-profile-list",
+      "category": "server-profiles",
+      "count": 1,
+      "members": [
+          {
+            "type": "server-profiles",
+            "name": "SP1",
+            "status": "Critical",
+          }]
+    };
+    sinon.stub(oVClient.connection, '__http__').returns(Bluebird.resolve(serverProfileResponse));
+
+    serverProfiles.getProfilesByStatus("Critical").then(function(data) {
+      data.members[0].status.should.equal("Critical");
+    });
+  }));
+
+  it('get warning profiles', sinon.test(function() {
+    let serverProfileResponse = {
+      "type": "server-profile-list",
+      "category": "server-profiles",
+      "count": 1,
+      "members": [
+          {
+            "type": "server-profiles",
+            "name": "SP1",
+            "status": "Warning",
+          }]
+    };
+    sinon.stub(oVClient.connection, '__http__').returns(Bluebird.resolve(serverProfileResponse));
+
+    serverProfiles.getProfilesByStatus("Warning").then(function(data) {
+      data.members[0].status.should.equal("Warning");
+    });
+  }));
+
+  it('get OK profiles', sinon.test(function() {
+    let serverProfileResponse = {
+      "type": "server-profile-list",
+      "category": "server-profiles",
+      "count": 1,
+      "members": [
+          {
+            "type": "server-profiles",
+            "name": "SP1",
+            "status": "OK",
+          }]
+    };
+    sinon.stub(oVClient.connection, '__http__').returns(Bluebird.resolve(serverProfileResponse));
+
+    serverProfiles.getProfilesByStatus("OK").then(function(data) {
+      data.members[0].status.should.equal("OK");
+    });
+  }));
+
+  it('get disabled profiles', sinon.test(function() {
+    let serverProfileResponse = {
+      "type": "server-profile-list",
+      "category": "server-profiles",
+      "count": 1,
+      "members": [
+          {
+            "type": "server-profiles",
+            "name": "SP1",
+            "status": "Disabled",
+          }]
+    };
+    sinon.stub(oVClient.connection, '__http__').returns(Bluebird.resolve(serverProfileResponse));
+
+    serverProfiles.getProfilesByStatus("Disabled").then(function(data) {
+      data.members[0].status.should.equal("Disabled");
+    });
+  }));
+
+
+
+});
