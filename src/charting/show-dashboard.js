f3d92ddd5a6af184ab2fb3ca58c47f33f45e917b
U-AMERICAS\cribari <cribari@DOBJQF2TLC.americas.hpqcorp.net>
2017-07-14 10:57:50 -0600

    Refactor OV Dashboard code

--- src/charting/show-dashboard.js
+++ src/charting/show-dashboard.js
@@ -53,83 +53,162 @@ function __uploadPNG__(robot, room, fileName) {
   });
 }
 
+function __transformProfileData__(aggregatedServerProfiles) {
+  let splits1 = aggregatedServerProfiles[0].counts.length;
+  let dataset1 = [];
+  let totalCount1 = 0;
+  if (splits1 === 0) {
+    dataset1.push({ label: "No Profiles", count: 0, percent: 100} );
+  } else {
+    let slices1 = [];
+    for (let i = 0; i < splits1; i++) {
+      slices1.push(aggregatedServerProfiles[0].counts[i]);
+      totalCount1 += aggregatedServerProfiles[0].counts[i].count;
+    }
+    for (let j = 0; j < splits1; j++) {
+      dataset1.push({ label: slices1[j].value, count: (slices1[j].count), percent: (slices1[j].count / totalCount1) });
+    };
+  }
+
+  return { splits: splits1, totalCount: totalCount1, dataset: dataset1 };
+}
+
+function __transformHardwareData__(aggregatedServerHardware) {
+  let splits2 = aggregatedServerHardware[0].counts.length;
+  let dataset2 = [];
+  let totalCount2 = 0;
+  if (splits2 === 0) {
+    dataset2.push( { label: "No Hardware", count: 0, percent: 100} );
+  } else {
+    let slices2 = [];
+    for (let i = 0; i < splits2; i++) {
+      slices2.push(aggregatedServerHardware[0].counts[i]);
+      totalCount2 += aggregatedServerHardware[0].counts[i].count;
+    };
+    for (let j = 0; j < splits2; j++) {
+      dataset2.push({ label: slices2[j].value, count: (slices2[j].count), percent: (slices2[j].count / totalCount2) });
+    };
+  }
+
+  return { splits: splits2, totalCount: totalCount2, dataset: dataset2 };
+}
+
+function __transformAlertsData__(aggregatedAlerts) {
+  let splits3 = aggregatedAlerts[0].counts.length;
+  let dataset3 = [];
+  let totalCount3 = 0;
+  if (splits3 === 0) {
+    dataset3.push({ label: "No Alerts", count: 0, percent: 100} );
+  }
+  else {
+    let slices3 = [];
+    for (let i = 0; i < splits3; i++) {
+      slices3.push(aggregatedAlerts[0].counts[i]);
+      totalCount3 += aggregatedAlerts[0].counts[i].count;
+    };
+    for (let j = 0; j < splits3; j++) {
+      dataset3.push({ label: slices3[j].value, count: (slices3[j].count), percent: (slices3[j].count / totalCount3) });
+    };
+  }
+
+  return { splits: splits3, totalCount: totalCount3, dataset: dataset3 };
+}
+
+function __transformHardwareWithProfilesData(aggregatedHardwareWithProfiles) {
+  let splits4 = aggregatedHardwareWithProfiles[0].counts.length;
+  let dataset4 = [];
+  let totalCount4 = 0;
+  if (splits4 === 0) {
+    dataset4.push({ label: "No Hardware", count: 0, percent: 100} );
+  }
+  else {
+    let slices4 = [];
+    for (let i = 0; i < splits4; i++) {
+      slices4.push(aggregatedHardwareWithProfiles[0].counts[i]);
+      totalCount4 += aggregatedHardwareWithProfiles[0].counts[i].count;
+    }
+    for (let j = 0; j < splits4; j++) {
+      if (slices4[j].value == "ProfileApplied") {
+        dataset4.push({ label: "Has Profile", count: (slices4[j].count), percent: (slices4[j].count / totalCount4) });
+      }
+      else {
+        dataset4.push({ label: "No Profile", count: (slices4[j].count), percent: (slices4[j].count / totalCount4) });
+      }
+    };
+  }
+
+  return { splits: splits4, totalCount: totalCount4, dataset: dataset4 };
+}
+
+function __addHeadersAndLines(svg) {
+  //adding headers to each section and lines to separate in the center
+  svg.append("text")
+    .attr("x", 75)
+    .attr("y", 75)
+    .style("font-size", "3.0em")
+    .style("font-family", "'MetricHPE Black'")
+    .text("Server Profiles");
+
+  svg.append("text")
+    .attr("x", 680)
+    .attr("y", 75)
+    .style("font-size", "3.0em")
+    .style("font-family", "'MetricHPE Black'")
+    .text("Server Hardware");
+
+  svg.append("text")
+    .attr("x", 75)
+    .attr("y", 700)
+    .style("font-size", "3.0em")
+    .style("font-family", "'MetricHPE Black'")
+    .text("Apppliance Alerts");
+
+  svg.append("text")
+    .attr("x", 680)
+    .attr("y", 700)
+    .style("font-size", "3.0em")
+    .style("font-family", "'MetricHPE Black'")
+    .text("Servers with profiles");
+
+  svg.append("svg:line")
+    .attr("x1", 75)
+    .attr("x2", 550)
+    .attr("y1", 575)
+    .attr("y2", 575)
+    .style("stroke", "rgb(189, 189, 189)");
+
+  svg.append("svg:line")
+    .attr("x1", 680)
+    .attr("x2", 1155)
+    .attr("y1", 575)
+    .attr("y2", 575)
+    .style("stroke", "rgb(189, 189, 189)");
+}
+
 export function buildDashboard(robot, room, aggregatedAlerts, aggregatedServerProfiles, aggregatedServerHardware, aggregatedHardwareWithProfiles) {
 
   return new Promise((resolve, reject) => {
-    //separate data for server profiles
-    let splits1 = aggregatedServerProfiles[0].counts.length;
-    let dataset1 = [];
-    let totalCount1 = 0;
-    if (splits1 === 0) {
-      dataset1.push({ label: "No Profiles", count: 0, percent: 100} );
-    } else {
-      let slices1 = [];
-      for (let i = 0; i < splits1; i++) {
-        slices1.push(aggregatedServerProfiles[0].counts[i]);
-        totalCount1 += aggregatedServerProfiles[0].counts[i].count;
-      }
-      for (let j = 0; j < splits1; j++) {
-        dataset1.push({ label: slices1[j].value, count: (slices1[j].count), percent: (slices1[j].count / totalCount1) });
-      };
-    }
 
-    //separate data for server hardware
-    let splits2 = aggregatedServerHardware[0].counts.length;
-    let dataset2 = [];
-    let totalCount2 = 0;
-    if (splits2 === 0) {
-      dataset2.push( { label: "No Hardware", count: 0, percent: 100} );
-    } else {
-      let slices2 = [];
-      for (let i = 0; i < splits2; i++) {
-        slices2.push(aggregatedServerHardware[0].counts[i]);
-        totalCount2 += aggregatedServerHardware[0].counts[i].count;
-      };
-      for (let j = 0; j < splits2; j++) {
-        dataset2.push({ label: slices2[j].value, count: (slices2[j].count), percent: (slices2[j].count / totalCount2) });
-      };
-    }
+    //split the data for pie generation
+    let profiles = __transformProfileData__(aggregatedServerProfiles);
+    let splits1 = profiles.splits;
+    let totalCount1 = profiles.totalCount;
+    let dataset1 = profiles.dataset;
 
-    //separate data for alerts
-    let splits3 = aggregatedAlerts[0].counts.length;
-    let dataset3 = [];
-    let totalCount3 = 0;
-    if (splits3 === 0) {
-      dataset3.push({ label: "No Alerts", count: 0, percent: 100} );
-    }
-    else {
-      let slices3 = [];
-      for (let i = 0; i < splits3; i++) {
-        slices3.push(aggregatedAlerts[0].counts[i]);
-        totalCount3 += aggregatedAlerts[0].counts[i].count;
-      };
-      for (let j = 0; j < splits3; j++) {
-        dataset3.push({ label: slices3[j].value, count: (slices3[j].count), percent: (slices3[j].count / totalCount3) });
-      };
-    }
+    let hardware = __transformHardwareData__(aggregatedServerHardware);
+    let splits2 = hardware.splits;
+    let totalCount2 = hardware.totalCount;
+    let dataset2 = hardware.dataset;
 
-    //separate data for hardware with profiles
-    let splits4 = aggregatedHardwareWithProfiles[0].counts.length;
-    let dataset4 = [];
-    let totalCount4 = 0;
-    if (splits4 === 0) {
-      dataset4.push({ label: "No Hardware", count: 0, percent: 100} );
-    }
-    else {
-      let slices4 = [];
-      for (let i = 0; i < splits4; i++) {
-        slices4.push(aggregatedHardwareWithProfiles[0].counts[i]);
-        totalCount4 += aggregatedHardwareWithProfiles[0].counts[i].count;
-      }
-      for (let j = 0; j < splits4; j++) {
-        if (slices4[j].value == "ProfileApplied") {
-          dataset4.push({ label: "Has Profile", count: (slices4[j].count), percent: (slices4[j].count / totalCount4) });
-        }
-        else {
-          dataset4.push({ label: "No Profile", count: (slices4[j].count), percent: (slices4[j].count / totalCount4) });
-        }
-      };
-    }
+    let alerts = __transformAlertsData__(aggregatedAlerts);
+    let splits3 = alerts.splits;
+    let totalCount3 = alerts.totalCount;
+    let dataset3 = alerts.dataset;
+
+    let hardwareWithProfiles = __transformHardwareWithProfilesData(aggregatedHardwareWithProfiles);
+    let splits4 = hardwareWithProfiles.splits;
+    let totalCount4 = hardwareWithProfiles.totalCount;
+    let dataset4 = hardwareWithProfiles.dataset;
 
     //generate the document to write the svg
     const document = jsdom.jsdom("<html><body></body></html>");
@@ -138,29 +217,13 @@ export function buildDashboard(robot, room, aggregatedAlerts, aggregatedServerPr
     //set the radius of the donut (the center part)
     let radius = 200;
 
-    //color scheme used for status and default donuts
+    //color scheme
     let color1 = d3.scaleOrdinal()
-        .domain(["Critical", "OK", "Warning", "No Profiles", "No Alerts", "No Hardware"])
-        .range(['#FF454F', '#01A982', '#FFD042', '#C6C9CA', '#C6C9CA', '#C6C9CA']);
-    //color scheme used for hardware with profiles
-    let color2 = d3.scaleOrdinal()
-        .domain(["Has Profile", "No Profile"])
-        .range(["#425563", "#C6C9CA"])
-
-    //arc for server profiles
-    let arc1 = d3.arc()
-        .outerRadius(radius * 0.8)
-        .innerRadius(radius * 0.6);
-    //arc for server hardware
-    let arc2 = d3.arc()
-        .outerRadius(radius * 0.8)
-        .innerRadius(radius * 0.6);
-    //arc for alerts
-    let arc3 = d3.arc()
-        .outerRadius(radius * 0.8)
-        .innerRadius(radius * 0.6);
-    //arc for hardware with profiles
-    let arc4 = d3.arc()
+        .domain(["Critical", "OK", "Warning", "No Profiles", "No Alerts", "No Hardware", "Has Profile", "No Profile"])
+        .range(['#FF454F', '#01A982', '#FFD042', '#C6C9CA', '#C6C9CA', '#C6C9CA', '#425563', '#C6C9CA']);
+        
+    //arc used in the generation of all the pies
+    let arc = d3.arc()
         .outerRadius(radius * 0.8)
         .innerRadius(radius * 0.6);
     //outer arc used for calculating placement of labels for each individual section
@@ -168,20 +231,8 @@ export function buildDashboard(robot, room, aggregatedAlerts, aggregatedServerPr
         .outerRadius(radius * 0.9)
         .innerRadius(radius * 0.9);
 
-    //pie for server profiles
+    //generates all pie charts in dashboard
     let pie = d3.pie()
-        .sort(null)
-        .value(function(d) { return d.percent; });
-    //pie for server hardware
-    let pie2 = d3.pie()
-        .sort(null)
-        .value(function(d) { return d.percent; });
-    //pie for alerts
-    let pie3 = d3.pie()
-        .sort(null)
-        .value(function(d) { return d.percent; });
-    //pie for hardware with profiles
-    let pie4 = d3.pie()
         .sort(null)
         .value(function(d) { return d.percent; });
     //dimensions of the svg (and ultimately the png )
@@ -215,48 +266,8 @@ export function buildDashboard(robot, room, aggregatedAlerts, aggregatedServerPr
         .append("g")
         .attr("transform", "translate(" + (radius + 700) + "," + (radius + 700) + ")");
 
-    //adding headers to each section and lines to separate in the center
-    svg.append("text")
-      .attr("x", 75)
-      .attr("y", 75)
-      .style("font-size", "3.0em")
-      .style("font-family", "'MetricHPE Black'")
-      .text("Server Profiles");
-
-    svg.append("text")
-      .attr("x", 680)
-      .attr("y", 75)
-      .style("font-size", "3.0em")
-      .style("font-family", "'MetricHPE Black'")
-      .text("Server Hardware");
-
-    svg.append("text")
-      .attr("x", 75)
-      .attr("y", 700)
-      .style("font-size", "3.0em")
-      .style("font-family", "'MetricHPE Black'")
-      .text("Apppliance Alerts");
-
-    svg.append("text")
-      .attr("x", 680)
-      .attr("y", 700)
-      .style("font-size", "3.0em")
-      .style("font-family", "'MetricHPE Black'")
-      .text("Servers with profiles");
-
-    svg.append("svg:line")
-      .attr("x1", 75)
-      .attr("x2", 550)
-      .attr("y1", 575)
-      .attr("y2", 575)
-      .style("stroke", "rgb(189, 189, 189)");
-
-    svg.append("svg:line")
-      .attr("x1", 680)
-      .attr("x2", 1155)
-      .attr("y1", 575)
-      .attr("y2", 575)
-      .style("stroke", "rgb(189, 189, 189)");
+    //adds headers for each section and stylistic lines to svg
+    __addHeadersAndLines(svg);
 
     //generate the profiles donut
     let path = g1.selectAll(".arc")
@@ -264,7 +275,7 @@ export function buildDashboard(robot, room, aggregatedAlerts, aggregatedServerPr
         .enter()
         .append("path")
         .attr("fill", function(d, i) { return color1(d.data.label); })
-        .attr("d", arc1);
+        .attr("d", arc);
 
     //finds the center of the arc to place the polyline
     function midAngle(d) { return d.startAngle + (d.endAngle - d.startAngle) / 2; }
@@ -303,7 +314,7 @@ export function buildDashboard(robot, room, aggregatedAlerts, aggregatedServerPr
           .attr('points', function(d) {
             let pos = outerArc.centroid(d);
             pos[0] = radius * 0.95 * (midAngle(d) < Math.PI ? 1 : -1);
-            return [arc1.centroid(d), outerArc.centroid(d), pos];
+            return [arc.centroid(d), outerArc.centroid(d), pos];
           });
 
         }
@@ -323,11 +334,11 @@ export function buildDashboard(robot, room, aggregatedAlerts, aggregatedServerPr
         .text("Total");
 
     path = g2.selectAll(".arc")
-        .data(pie2(dataset2))
+        .data(pie(dataset2))
         .enter()
         .append("path")
         .attr("fill", function(d, i) { return color1(d.data.label); })
-        .attr("d", arc2);
+        .attr("d", arc);
 
     if (splits2 != 0) {
       let label2 = g2.selectAll('text')
@@ -361,7 +372,7 @@ export function buildDashboard(robot, room, aggregatedAlerts, aggregatedServerPr
           .attr('points', function(d) {
             let pos = outerArc.centroid(d);
             pos[0] = radius * 0.95 * (midAngle(d) < Math.PI ? 1 : -1);
-            return [arc2.centroid(d), outerArc.centroid(d), pos];
+            return [arc.centroid(d), outerArc.centroid(d), pos];
           });
     }
 
@@ -381,11 +392,11 @@ export function buildDashboard(robot, room, aggregatedAlerts, aggregatedServerPr
         .text("Total");
 
     path = g3.selectAll(".arc")
-        .data(pie3(dataset3))
+        .data(pie(dataset3))
         .enter()
         .append("path")
         .attr("fill", function(d, i) { return color1(d.data.label); })
-        .attr("d", arc3);
+        .attr("d", arc);
     if (splits3 != 0) {
       let label3 = g3.selectAll('text')
             .data(pie(dataset3))
@@ -419,7 +430,7 @@ export function buildDashboard(robot, room, aggregatedAlerts, aggregatedServerPr
           .attr('points', function(d) {
             let pos = outerArc.centroid(d);
             pos[0] = radius * 0.95 * (midAngle(d) < Math.PI ? 1 : -1);
-            return [arc3.centroid(d), outerArc.centroid(d), pos];
+            return [arc.centroid(d), outerArc.centroid(d), pos];
           });
     }
 
@@ -438,11 +449,11 @@ export function buildDashboard(robot, room, aggregatedAlerts, aggregatedServerPr
         .text("Total");
 
     path = g4.selectAll(".arc")
-        .data(pie3(dataset4))
+        .data(pie(dataset4))
         .enter()
         .append("path")
-        .attr("fill", function(d, i) { return color2(d.data.label); })
-        .attr("d", arc4);
+        .attr("fill", function(d, i) { return color1(d.data.label); })
+        .attr("d", arc);
     if (splits4 != 0) {
       let label4 = g4.selectAll('text')
             .data(pie(dataset4))
@@ -476,7 +487,7 @@ export function buildDashboard(robot, room, aggregatedAlerts, aggregatedServerPr
           .attr('points', function(d) {
             let pos = outerArc.centroid(d);
             pos[0] = radius * 0.95 * (midAngle(d) < Math.PI ? 1 : -1);
-            return [arc4.centroid(d), outerArc.centroid(d), pos];
+            return [arc.centroid(d), outerArc.centroid(d), pos];
           });
     }
 
